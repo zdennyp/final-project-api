3e805305dd560650908b00dc2199c8b7903e89a6
zdennyp <zdenekpetera77@gmail.com>
2021-03-03 16:53:43 +0100

    uploading to repository

---
+++ controllers/image.js
@@ -0,0 +1,28 @@
+const { json } = require('body-parser');
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '73e218ca954d44fb9946fd171e11adda'
+   });
+
+const handleApiCall = (req, res) => {
+    app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+        res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const handleImage = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = { handleImage, handleApiCall };
